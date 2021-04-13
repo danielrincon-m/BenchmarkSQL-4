0378f80cf9fb06f3955ce95aa92ed130d2db0e26
Daniel R <daniel.rincon96@gmail.com>
2021-04-12 20:14:08 -0500

    Indices

--- src/main/resources/sqlserver/sqlIndexCreates.sql
+++ src/main/resources/sqlserver/sqlIndexCreates.sql
@@ -5,15 +5,14 @@ alter table benchmarksql.warehouse add constraint pk_warehouse
 
 alter table benchmarksql.district add constraint pk_district 
   primary key (d_w_id, d_id);
-
--- create nonclustered index idx_district_d_id on benchmarksql.district (d_id);
--- create nonclustered index idx_district_d_w_id on benchmarksql.district (d_w_id);
+create nonclustered index idx_district_d_id on benchmarksql.district (d_id);
+create nonclustered index idx_district_d_w_id on benchmarksql.district (d_w_id);
 
 alter table benchmarksql.customer add constraint pk_customer 
   primary key (c_w_id, c_d_id, c_id);
--- create nonclustered index idx_customer_c_w_id on benchmarksql.customer (c_w_id);
--- create nonclustered index idx_customer_c_d_id on benchmarksql.customer (c_d_id);
--- create nonclustered index idx_customer_c_id on benchmarksql.customer (c_id);
+create nonclustered index idx_customer_c_w_id on benchmarksql.customer (c_w_id);
+create nonclustered index idx_customer_c_d_id on benchmarksql.customer (c_d_id);
+create nonclustered index idx_customer_c_id on benchmarksql.customer (c_id);
 
 alter table benchmarksql.history add constraint pk_history 
   primary key (hist_id);
@@ -26,15 +25,27 @@ exec('ALTER SEQUENCE hist_id_seq RESTART WITH ' + @max);
 
 alter table benchmarksql.oorder add constraint pk_oorder 
   primary key (o_w_id, o_d_id, o_id);
+create nonclustered index idx_oorder_o_w_id on benchmarksql.oorder (o_w_id);
+create nonclustered index idx_oorder_o_d_id on benchmarksql.oorder (o_d_id);
+create nonclustered index idx_oorder_o_id on benchmarksql.oorder (o_id);
  
 alter table benchmarksql.new_order add constraint pk_new_order 
   primary key (no_w_id, no_d_id, no_o_id);
+create nonclustered index idx_new_order_no_w_id on benchmarksql.new_order (no_w_id);
+create nonclustered index idx_new_order_no_d_id on benchmarksql.new_order (no_d_id);
+create nonclustered index idx_new_order_no_o_id on benchmarksql.new_order (no_o_id);
 
 alter table benchmarksql.order_line add constraint pk_order_line 
   primary key (ol_w_id, ol_d_id, ol_o_id, ol_number);
+create nonclustered index idx_order_line_ol_w_id on benchmarksql.order_line (ol_w_id);
+create nonclustered index idx_order_line_ol_d_id on benchmarksql.order_line (ol_d_id);
+create nonclustered index idx_order_line_ol_o_id on benchmarksql.order_line (ol_o_id);
+create nonclustered index idx_order_line_ol_number on benchmarksql.order_line (ol_number);
 
 alter table benchmarksql.stock add constraint pk_stock
   primary key (s_w_id, s_i_id);
+create nonclustered index idx_stock_s_w_id on benchmarksql.stock (s_w_id);
+create nonclustered index idx_stock_s_i_id on benchmarksql.stock (s_i_id);
 
 alter table benchmarksql.item add constraint pk_item
   primary key (i_id);
--- src/main/resources/sqlserver/sqlIndexDrops.sql
+++ src/main/resources/sqlserver/sqlIndexDrops.sql
@@ -18,13 +18,25 @@ drop index if exists benchmarksql.customer.idx_customer_c_id;
 alter table benchmarksql.history drop constraint pk_history;
 
 alter table benchmarksql.oorder drop constraint pk_oorder;
+drop index if exists benchmarksql.oorder.idx_oorder_o_w_id;
+drop index if exists benchmarksql.oorder.idx_oorder_o_d_id;
+drop index if exists benchmarksql.oorder.idx_oorder_o_id;
 
 -- drop index ndx_oorder_carrier;
 
 alter table benchmarksql.new_order drop constraint pk_new_order;
+drop index if exists benchmarksql.new_order.idx_new_order_no_w_id;
+drop index if exists benchmarksql.new_order.idx_new_order_no_d_id;
+drop index if exists benchmarksql.new_order.idx_new_order_no_o_id;
 
 alter table benchmarksql.order_line drop constraint pk_order_line;
+drop index if exists benchmarksql.order_line.idx_order_line_ol_w_id;
+drop index if exists benchmarksql.order_line.idx_order_line_ol_d_id;
+drop index if exists benchmarksql.order_line.idx_order_line_ol_o_id;
+drop index if exists benchmarksql.order_line.idx_order_line_ol_number;
 
 alter table benchmarksql.stock drop constraint pk_stock;
+drop index if exists benchmarksql.stock.idx_stock_s_w_id;
+drop index if exists benchmarksql.stock.idx_stock_s_i_id;
 
 alter table benchmarksql.item drop constraint pk_item;
